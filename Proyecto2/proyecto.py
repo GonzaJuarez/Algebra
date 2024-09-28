e199da45c2994f15ba65c64d36260e4b956d10d6
JPAmorin <109167051+JPAmorin@users.noreply.github.com>
2024-09-28 15:36:35 -0300

    7 y 8

--- Proyecto2/proyecto.py
+++ Proyecto2/proyecto.py
@@ -2,7 +2,16 @@ from skimage.io import imread, imshow, imsave
 from skimage.util import img_as_ubyte
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+# Get the directory of the current Python file
+base_dir = os.path.dirname(__file__)
+
+# Construct relative paths for the images
+imagen1_path = os.path.join(base_dir, 'imagenes', 'lobo.jpg')
+imagen2_path = os.path.join(base_dir, 'imagenes', 'perico.jpg')
+imagen1_crop_path = os.path.join(base_dir, 'imagenes', 'lobo_crop.jpg')
+imagen2_crop_path = os.path.join(base_dir, 'imagenes', 'perico_crop.jpg')
 
 # Funciones
 def recortar_imagen(image, ruta_img_crop, x_inicial, x_final, y_inicial, y_final):
@@ -17,18 +26,18 @@ def recortar_imagen(image, ruta_img_crop, x_inicial, x_final, y_inicial, y_final
         print("Ha ocurrido un error:", str(e))
 
 def image_to_matiz(image):
-  try:
-    matriz_imagen = np.array(image)
-    return matriz_imagen
-  except Exception as e:
-    print("Ha ocurrido un error:", str(e))
+    try:
+        matriz_imagen = np.array(image)
+        return matriz_imagen
+    except Exception as e:
+        print("Ha ocurrido un error:", str(e))
 
 def transpuesta(matriz):
-  try:
-    matriz_transpuesta = np.transpose(matriz, (1, 0, 2))
-    return matriz_transpuesta
-  except Exception as e:
-    print("Ha ocurrido un error:", str(e))
+    try:
+        matriz_transpuesta = np.transpose(matriz, (1, 0, 2))
+        return matriz_transpuesta
+    except Exception as e:
+        print("Ha ocurrido un error:", str(e))
 
 def convertir_a_escala_grises(imagen_color: np.ndarray) -> np.ndarray:
     try:
@@ -54,18 +63,23 @@ def calcular_inversa_matriz(imagen_gris: np.ndarray) -> np.ndarray:
         print("La matriz no tiene inversa.")
         return None
 
+def imagen_por_escalar(imagen_gris: np.ndarray, alpha: float) -> np.ndarray:
+    # Multiplicar la imagen por el escalar alpha
+    imagen_ajustada = imagen_gris * alpha
+    # Asegurarse de que los valores están en el rango 0-255
+    imagen_ajustada = np.clip(imagen_ajustada, 0, 255).astype(np.uint8)
+    return imagen_ajustada
+
 
 # Variables
+imagen1 = imread(imagen1_path)
+imagen2 = imread(imagen2_path)
 
-imagen1 = imread('C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/lobo.jpg')
-imagen2 = imread('C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/perico.jpg')
+recortar_imagen(imagen1, imagen1_crop_path, 0, 400, 400, 800)
+recortar_imagen(imagen2, imagen2_crop_path, 0, 400, 0, 400)
 
-recortar_imagen(imagen1, 'C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/lobo_crop.jpg', 0, 400, 400, 800)
-
-recortar_imagen(imagen2, 'C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/perico_crop.jpg', 0, 400, 0, 400)
-
-imagen1_crop = imread('C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/lobo_crop.jpg')
-imagen2_crop = imread('C:/Users/Usuario/Desktop/Algebra/Algebra/Proyecto2/imagenes/perico_crop.jpg')
+imagen1_crop = imread(imagen1_crop_path)
+imagen2_crop = imread(imagen2_crop_path)
 
 imagen1_matriz = image_to_matiz(imagen1_crop)
 imagen2_matriz = image_to_matiz(imagen2_crop)
@@ -73,15 +87,13 @@ imagen2_matriz = image_to_matiz(imagen2_crop)
 imagen1_traspuesta = transpuesta(imagen1_matriz)
 imagen2_traspuesta = transpuesta(imagen2_matriz)
 
-imagen1_gris = convertir_a_escala_grises(imagen1_traspuesta)
-imagen2_gris = convertir_a_escala_grises(imagen2_traspuesta)
+imagen1_gris = convertir_a_escala_grises(imagen1_crop)
+imagen2_gris = convertir_a_escala_grises(imagen2_crop)
 
 imagen1_inversa = calcular_inversa_matriz(imagen1_gris)
 imagen2_inversa = calcular_inversa_matriz(imagen2_gris)
 
-
 # Prints
-
 print(f"Tamaño de la imagen 1: {imagen1.shape}")
 print(f"Tamaño de la imagen 2: {imagen2.shape}")
 
@@ -100,20 +112,13 @@ print(imagen2_matriz)
 
 print("")
 
-print(f"Tamaño de la imagen 1: {imagen1_traspuesta.shape}")
+print(f"Tamaño de la imagen 1 traspuesta: {imagen1_traspuesta.shape}")
+print(f"Tamaño de la imagen 2 traspuesta: {imagen2_traspuesta.shape}")
 
 print("")
 
-print("Imagen 1 traspuesta:")
-print(imagen1_traspuesta)
-
-print("Imagen 2 traspuesta:")
-print(imagen2_traspuesta)
-
-print("")
-
-print(f"Tamaño de la imagen 1: {imagen1_gris.shape}")
-print(f"Tamaño de la imagen 2: {imagen2_gris.shape}")
+print(f"Tamaño de la imagen 1 en escala de grises: {imagen1_gris.shape}")
+print(f"Tamaño de la imagen 2 en escala de grises: {imagen2_gris.shape}")
 
 print("")
 
@@ -124,10 +129,10 @@ print("Imagen 2 inversa:")
 print(imagen2_inversa)
 
 # Plots
-
 imshow(imagen1)
 plt.title("Imagen 1")
 plt.show()
+
 imshow(imagen2)
 plt.title("Imagen 2")
 plt.show()
@@ -135,6 +140,7 @@ plt.show()
 imshow(imagen1_crop)
 plt.title("Imagen 1 recortada")
 plt.show()
+
 imshow(imagen2_crop)
 plt.title("Imagen 2 recortada")
 plt.show()
@@ -142,13 +148,30 @@ plt.show()
 plt.imshow(imagen1_traspuesta)
 plt.title("Imagen 1 traspuesta")
 plt.show()
+
 plt.imshow(imagen2_traspuesta)
 plt.title("Imagen 2 traspuesta")
 plt.show()
 
-plt.imshow(imagen1_gris,  cmap='gray')
-plt.title("Imagen 1 traspuesta escala de grises")
+plt.imshow(imagen1_gris, cmap='gray')
+plt.title("Imagen 1 en escala de grises")
 plt.show()
-plt.imshow(imagen2_gris,  cmap='gray')
-plt.title("Imagen 2 traspuesta escala de grises")
+
+plt.imshow(imagen2_gris, cmap='gray')
+plt.title("Imagen 2 en escala de grises")
+plt.show()
+
+print(calcular_inversa_matriz(imagen1_gris))
+print(calcular_inversa_matriz(imagen2_gris))
+
+imagen1_gris_escalar_alto = imagen_por_escalar(imagen1_gris, 5)
+plt.subplot(2, 2, 1)
+plt.imshow(imagen1_gris_escalar_alto, cmap='gray')
+plt.title(f'Imagen 1 - Escalar (α={5})')
+plt.show()
+
+imagen1_gris_escalar_bajo = imagen_por_escalar(imagen1_gris, 0.2)
+plt.subplot(2, 2, 1)
+plt.imshow(imagen1_gris_escalar_bajo, cmap='gray')
+plt.title(f'Imagen 1 - Escalar (α={0.2})')
 plt.show()
